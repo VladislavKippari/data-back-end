ba9a5a184d96d6e48068766daced5bbb366af325
Vladislav Kipari <kippari.v@gmail.com>
2020-05-16 12:33:12 +0300

    hostdlt

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 var configValues=require('./config');
 module.exports={
     getDbConnectionString:function(){
-        return 'postgres://'+configValues.uname+':'+configValues.pwd+'@dev.vk.edu.ee:5432/db_Kippari';
+        return 'postgres://'+configValues.uname+':'+configValues.pwd+'@:/';
     }
 }
 //  'postgres://{db_username}:{db_password}@{host}:{port}/{db_name}'
